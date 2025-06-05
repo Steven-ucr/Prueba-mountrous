1326fae1c6d4c3d01813f4728e76ad4e87c8b718
Steven <carlos.mendezmayorga@ucr.ac.cr>
2025-06-05 00:44:20 -0600

    mi primer commit

---
+++ Combate.cs
@@ -0,0 +1,57 @@
+﻿
+public class Combate
+{
+    public static void Main(string[] args)
+    {
+       
+        Mounstrou mons_1 = new Mounstrou();
+        Mounstrou mons_2 = new Mounstrou();
+        mons_1.ImprimirInformacion();
+        mons_2.ImprimirInformacion();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //Console.WriteLine("Ingrese el nombre del Mountrou");
+   //String nombre=Console.ReadLine();
+   //Console.WriteLine("Ingrese el tipo de Mountrou");
+   //String tipo=Console.ReadLine();
+   //Console.WriteLine("Ingrese la vida del Mountrou");
+   //int vida=int.Parse(Console.ReadLine());
+   //Console.WriteLine("Ingrese la fuerza del mounstrou");
+   //int fuerza = int.Parse(Console.ReadLine());
+   //Mounstrou moun_1 = new Mounstrou (nombre, tipo, vida, fuerza);
+   // Console.WriteLine("Ingrese el nombre del Mountrou 2");
+   //String nombreq = Console.ReadLine();
+   //Console.WriteLine("Ingrese el tipo de Mountrou 2");
+   //String tipoq = Console.ReadLine();
+   //Console.WriteLine("Ingrese la vida del Mountrou 2");
+   //int vidaq = int.Parse(Console.ReadLine());
+   //Console.WriteLine("Ingrese la fuerza del mounstrou 2");
+   //int fuerzaq = int.Parse(Console.ReadLine());
+   // Mounstrou moun_2 = new Mounstrou(nombreq, tipoq, vidaq, fuerzaq);
---
+++ Mounstrou.cs
@@ -0,0 +1,65 @@
+﻿using System;
+// la clase de mounstrous 
+public class Mounstrou
+{
+    public string nombre;
+    public string tipo;
+    public int vida;
+    public int fuerza;
+
+
+    // el constructor creo
+    public Mounstrou()
+    {
+        this.nombre = PedirNombre();
+        this.tipo = PedirTipo();
+        this.vida = PedirVida();
+        this.fuerza = PedirFuerza();
+
+    }
+    public string PedirNombre()
+    {
+        Console.Write("Ingrese el nombre de su mounstruo:");
+        string nombre = "" + Console.ReadLine();
+        return nombre;
+    }
+    public string PedirTipo()
+    {
+        Console.Write($"Ingrese si {this.nombre} es de tipo Fuego, Agua, Aire o Tierra:");
+        string tipo = "" + Console.ReadLine();
+        return tipo;
+    }
+    public int PedirVida()
+    {
+        Console.Write($"ingrese la vida de {this.nombre} del tipo {this.tipo}:");
+        int vida = int.Parse(Console.ReadLine());
+        return vida;
+    }
+    public int PedirFuerza()
+    {
+        Console.Write($"ingrese la fuerza de {this.nombre} del tipo {this.tipo}:");
+        int fuerza = int.Parse(Console.ReadLine());
+        return fuerza;
+    }
+    public void ImprimirInformacion()
+    { 
+        Console.WriteLine($"{this.nombre} es del tipo {this.tipo} con vida {this.vida} y fuerza {this.fuerza}");
+    }
+}
+
+
+
+
+//public string nombreq;
+//public string tipoq;
+//public int vidaq;
+//public int fuerzaq;
+
+
+
+
+// this.nombreq = nombreq;
+//this.tipoq = tipoq;
+//this.vidaq = vidaq;
+//this.fuerzaq = fuerzaq;
+
